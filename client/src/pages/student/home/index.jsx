a26734e58bfde32f1c9de3970592fdbf08c59c01
Thanh Nguyen <thnhnguyn761@gmail.com>
2024-10-14 10:55:52 +0700

    Add student/home

--- client/src/pages/student/home/index.jsx
+++ client/src/pages/student/home/index.jsx
@@ -3,8 +3,21 @@ import React from "react";
 function StudentHomePage() {
   return (
     <div className="min-h-screen bg-white">
-      <section className="flex flex-col items-center justify-center py-8 px-4">
-        <h1 className="text-4xl font-bold">This is student home page</h1>
+      <section className="flex flex-col lg:flex-row items-center justify-between py-8 px-4 lg:px-8">
+        <div className="lg:w-1/2 lg:pr-12">
+          <h1 className="text-4xl font-bold mb-4">Learning thet gets you</h1>
+          <p className="text-xl">
+            Skills for your present and your future. Get Started with US
+          </p>
+        </div>
+        <div className="lg:w-full mb-8 lg:mb-0">
+          <img
+            src={banner}
+            width={600}
+            height={400}
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
+        </div>
       </section>
     </div>
   );
