98c3d900dd38ad53eee878dc41f7b31996b8124a
Thanh Nguyen <thnhnguyn761@gmail.com>
2024-11-09 22:15:27 +0700

    Update student/course-details

--- client/src/pages/student/course-details/index.jsx
+++ client/src/pages/student/course-details/index.jsx
@@ -1,11 +1,26 @@
-import { AuthContext } from "@/context/auth-context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-function StudentHomePage() {
-    const { studentViewCoursesList, setStudentViewCoursesList } =
-        useContext(StudentContext);
-    const { auth } = useContext(AuthContext)
+function StudentViewCourseDetailsPage() {
+    const {
+        studentViewCourseDetails,
+        setStudentViewCourseDetails,
+        currentCourseDetailsId,
+        setCurrentCourseDetailsId,
+        loadingState,
+        setLoadingState,
+    } = useContext(StudentContext);
+
+    const { auth } = useContext(Auth)
+
+    const [displayCurrentVideoFreePreview, setDisplayCurrentVideoFreePreview]
+        = useState(null);
+    const [showFreePreviewDialog, setShowFreePreviewDialog] = useState(false);
+    const [approvalUrl, setApprovalUrl] = useState("");
+    const navigate = useNavigate();
+    const { id } = useParams;
+    const location = useLocation();
     return (  );
 }
 
-export default StudentHomePage;
+export default StudentViewCourseDetailsPage;
