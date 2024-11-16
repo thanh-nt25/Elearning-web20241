98aa875ba53057d62d46ecc82c28605134389721
hpbchuot <thinh1212003@gmail.com>
2024-11-17 00:19:22 +0700

    feat/course

---
+++ server/test/route/course_route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+// router.get("/get", getAllStudentViewCourses);
+// router.get("/get/details/:id", getStudentViewCourseDetails);
+// router.get("/purchase-info/:id/:studentId", checkCoursePurchaseInfo);
+
+// module.exports = router;
