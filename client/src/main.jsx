50f2484cb1d5bf06b72441d0ae4c47738994e727
thanh-nt25 <thanhnguyenqwe65@gmail.com>
2024-11-18 16:38:50 +0700

    add login service

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import AuthProVider from './context/auth-context/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <AuthProVider>
+      <App />
+    </AuthProVider>
+  </BrowserRouter>
+    
 )
