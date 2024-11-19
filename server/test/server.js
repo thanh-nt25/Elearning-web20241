2efff82b92f32a216f882d0513e72231b015950c
NgocThanh2306 <shirotakuroqb@gmail.com>
2024-11-19 10:00:00 +0700

    test admin controller

--- server/test/server.js
+++ server/test/server.js
@@ -1,123 +1,136 @@
+const User = require("../../models/User");
 const Course = require("../../models/Course");
-
-
-//Add new course//
-const addNewCourse = async (req, res) => {
+// Add new user //
+const addNewUser = async (req, res) => {
   try {
-    const courseData = req.body;
-    const newlyCreatedCourse = new Course(courseData);
-    const saveCourse = await newlyCreatedCourse.save();
+    const userData = req.body;
+    const newUser = new User(userData);
+    const savedUser = await newUser.save();
 
-    if (saveCourse) {
+    if (savedUser) {
       res.status(201).json({
         success: true,
-        message: "Course saved successfully",
-        data: saveCourse,
+        message: "User added successfully",
+        data: savedUser,
       });
     }
   } catch (e) {
-    console.log(e);
+    console.error(e);
     res.status(500).json({
       success: false,
-      message: "Some error occured!",
+      message: "Some error occurred while adding the user!",
     });
   }
 };
-
-// Get all courses//
-const getAllCourses = async (req, res) => {
-
+// Get all user //
+const getAllUsers = async (req, res) => {
+    try {
+      const usersList = await User.find({});
   
-  try {
-
-
-    
-    const coursesList = await Course.find({  });
-
-    if (!coursesList || coursesList.length === 0) {
-      return res.status(200).json({
+      if (!usersList || usersList.length === 0) {
+        return res.status(200).json({
+          success: true,
+          data: [],
+          message: "No users found!",
+        });
+      }
+  
+      const usersWithCourseInfo = await Promise.all(
+        usersList.map(async (user) => {
+          if (user.role === "instructor") {
+            const createdCoursesCount = await Course.countDocuments({
+              instructorId: user._id.toString(),
+            });
+            return {
+              ...user.toObject(),
+              courseCount: createdCoursesCount, 
+            };
+          } else {
+            const enrolledCoursesCount = await Course.countDocuments({
+              "students.studentId": user._id.toString(),
+            });
+            return {
+              ...user.toObject(),
+              courseCount: enrolledCoursesCount, 
+            };
+          }
+        })
+      );
+      console.log(usersWithCourseInfo);
+      
+      res.status(200).json({
         success: true,
-        data: [],
-        message: "Request course list fail for admin",
+        data: usersWithCourseInfo,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        success: false,
+        message: "Some error occurred while fetching users!",
+      });
+    }
+  };
+
+  // Find users //
+const getUserDetailsByID = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userDetails = await User.findById(id);
+
+    if (!userDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found!",
       });
     }
 
     res.status(200).json({
       success: true,
-      data: coursesList,
+      data: userDetails,
     });
   } catch (e) {
     console.error(e);
     res.status(500).json({
       success: false,
-      message: "Some error occurred!",
+      message: "Some error occurred while fetching user details!",
     });
   }
 };
 
-//Find courses//
-const getCourseDetailsByID = async (req, res) => {
+// Update users //
+const updateUserByID = async (req, res) => {
   try {
     const { id } = req.params;
-    const courseDetails = await Course.findById(id);
+    const updatedUserData = req.body;
 
-    if (!courseDetails) {
+    const updatedUser = await User.findByIdAndUpdate(id, updatedUserData, {
+      new: true,
+    });
+
+    if (!updatedUser) {
       return res.status(404).json({
         success: false,
-        message: "Course not found!",
+        message: "User not found!",
       });
     }
 
     res.status(200).json({
       success: true,
-      data: courseDetails,
+      message: "User updated successfully",
+      data: updatedUser,
     });
   } catch (e) {
-    console.log(e);
+    console.error(e);
     res.status(500).json({
       success: false,
-      message: "Some error occured!",
-    });
-  }
-};
-
-
-// Update course//
-const updateCourseByID = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedCourseData = req.body;
-
-    const updatedCourse = await Course.findByIdAndUpdate(
-      id,
-      updatedCourseData,
-      { new: true }
-    );
-
-    if (!updatedCourse) {
-      return res.status(404).json({
-        success: false,
-        message: "Course not found!",
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      message: "Course updated successfully",
-      data: updatedCourse,
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      success: false,
-      message: "Some error occured!",
+      message: "Some error occurred while updating user!",
     });
   }
 };
 
 module.exports = {
-  addNewCourse,
-  getAllCourses,
-  updateCourseByID,
-  getCourseDetailsByID,
+  addNewUser,
+  getAllUsers,
+  getUserDetailsByID,
+  updateUserByID,
 };
