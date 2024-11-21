3898990a53a06d90fa7859c5e11ed5c89fe42dca
NgocThanh2306 <shirotakuroqb@gmail.com>
2024-11-21 10:00:00 +0700

    add adimin routes

--- server/routes/admin-routes/course-routes.js
+++ server/routes/admin-routes/course-routes.js
@@ -8,8 +8,11 @@ const {
 const router = express.Router();
 
 router.post("/add", addNewCourse);
+
 router.get("/get", getAllCourses);
+
 router.get("/get/details/:id", getCourseDetailsByID);
+
 router.put("/update/:id", updateCourseByID);
 
 module.exports = router;
