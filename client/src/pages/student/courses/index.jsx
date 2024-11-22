07756b8d5e8eea0678024dd16b5c284ecb1bb6e3
Thanh Nguyen <thnhnguyn761@gmail.com>
2024-11-22 08:15:42 +0700

    Update student/courses

--- client/src/pages/student/courses/index.jsx
+++ client/src/pages/student/courses/index.jsx
@@ -1,20 +1,77 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem } from "@/components/ui/dropdown-menu";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Label } from "@/components/ui/label";
 import { Skeleton } from "@/components/ui/skeleton";
 import { filterOptions, sortOptions } from "@/config";
-import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { AuthContext } from "@/context/auth-context";
+import { StudentContext } from "@/context/student-context";
+import {
+    checkCoursePurchaseInfoService,
+} from "@/services";
 import { ArrowUpDownIcon } from "lucide-react";
 import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function StudentViewCoursesPage() {
     const [sort, setSort] = useState("price-lowtohigh");
+    const [filters, setFilters] = useState({});
     const {
         studentViewCoursesList,
-        setStudentViewCoursesList,
         loadingState,
-        setLoadingState,
     } = useContext(StudentContext);
+    const navigate = useNavigate();
+    const { auth } = useContext(AuthContext);
+
+    function handleFilterOnChange(getSectionId, getCurrentOption) {
+        let cpyFilters = { ...filters };
+        const indexOfCurrentSeection =
+            Object.keys(cpyFilters).indexOf(getSectionId);
+
+        console.log(indexOfCurrentSeection, getSectionId);
+        if (indexOfCurrentSeection === -1) {
+            cpyFilters = {
+                ...cpyFilters,
+                [getSectionId]: [getCurrentOption.id],
+            };
+
+            console.log(cpyFilters);
+        } else {
+            const indexOfCurrentOption = cpyFilters[getSectionId].indexOf(
+                getCurrentOption.id
+            );
+
+            if (indexOfCurrentOption === -1)
+                cpyFilters[getSectionId].push(getCurrentOption.id);
+            else cpyFilters[getSectionId].splice(indexOfCurrentOption, 1);
+        }
+
+        setFilters(cpyFilters);
+        sessionStorage.setItem("filters", JSON.stringify(cpyFilters));
+    }
+
+    async function handleCourseNavigate(getCurrentCourseId) {
+        const response = await checkCoursePurchaseInfoService(
+            getCurrentCourseId,
+            auth?.user?._id
+        );
+
+        if (response?.success) {
+            if (response?.data) {
+                navigate(`/course-progress/${getCurrentCourseId}`);
+            } else {
+                navigate(`/course/details/${getCurrentCourseId}`);
+            }
+        }
+    }
 
     return (
         <div className="container mx-auto p-4">
@@ -22,34 +79,32 @@ function StudentViewCoursesPage() {
             <div className="flex flex-col md:flex-row gap-4">
                 <aside className="w-full md:w-64 space-y-4">
                     <div>
-                        {
-                            Object.keys(filterOptions).map((ketItem) => (
-                                <div className="p-4 border-b">
-                                    <h3 className="font-bold mb-3">{ketItem.toUpperCase()}</h3>
-                                    <div className="grid gap-2 mt-2">
-                                        {filterOptions[ketItem].map((option) => (
-                                            <Label className="flex font-medium items-center gap-3">
-                                                <Checkbox
-                                                    checked={
-                                                        filters &&
-                                                        Object.keys(filters).length > 0 &&
-                                                        filters[ketItem] &&
-                                                        filters[ketItem].indexOf(option.id) > -1
-                                                    }
-                                                    onCheckedChange={() =>
-                                                        handleFilterOnChange(ketItem, option)
-                                                    }
-                                                />
-                                                {option.label}
-                                            </Label>
-                                        ))}
-                                    </div>
+                        {Object.keys(filterOptions).map((ketItem) => (
+                            <div className="p-4 border-b">
+                                <h3 className="font-bold mb-3">{ketItem.toUpperCase()}</h3>
+                                <div className="grid gap-2 mt-2">
+                                    {filterOptions[ketItem].map((option) => (
+                                        <Label className="flex font-medium items-center gap-3">
+                                            <Checkbox
+                                                checked={
+                                                    filters &&
+                                                    Object.keys(filters).length > 0 &&
+                                                    filters[ketItem] &&
+                                                    filters[ketItem].indexOf(option.id) > -1
+                                                }
+                                                onCheckedChange={() =>
+                                                    handleFilterOnChange(ketItem, option)
+                                                }
+                                            />
+                                            {option.label}
+                                        </Label>
+                                    ))}
                                 </div>
-                            ))
-                        }
+                            </div>
+                        ))}
                     </div>
                 </aside>
-                <main>
+                <main className="flex-1">
                     <div className="flex justify-end items-center mb-4 gap-5">
                         <DropdownMenu>
                             <DropdownMenuTrigger asChild>
@@ -62,9 +117,11 @@ function StudentViewCoursesPage() {
                                     <span className="text-[16px] font-medium">Sort By</span>
                                 </Button>
                             </DropdownMenuTrigger>
-                            <DropdownMenuContent>
-                                <DropdownMenuRadioGroup value={sort}
-                                    onValueChange={(value) => setSort(value)}>
+                            <DropdownMenuContent align="end" className="w-[180px]">
+                                <DropdownMenuRadioGroup
+                                    value={sort}
+                                    onValueChange={(value) => setSort(value)}
+                                >
                                     {sortOptions.map((sortItem) => (
                                         <DropdownMenuRadioItem
                                             value={sortItem.id}
@@ -76,6 +133,9 @@ function StudentViewCoursesPage() {
                                 </DropdownMenuRadioGroup>
                             </DropdownMenuContent>
                         </DropdownMenu>
+                        <span className="text-sm text-black font-bold">
+                            {studentViewCoursesList.length} Results
+                        </span>
                     </div>
                     <div className="space-y-4">
                         {studentViewCoursesList && studentViewCoursesList.length > 0 ? (
@@ -121,7 +181,6 @@ function StudentViewCoursesPage() {
                             <h1 className="font-extrabold text-4xl">No Courses Found</h1>
                         )}
                     </div>
-
                 </main>
             </div>
         </div>
@@ -129,4 +188,3 @@ function StudentViewCoursesPage() {
 }
 
 export default StudentViewCoursesPage;
-
