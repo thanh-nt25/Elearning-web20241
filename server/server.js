35a4be27d9b078eef5f10a081df597bb4cdf957e
hpbchuot <thinh1212003@gmail.com>
2024-12-20 07:43:18 +0700

    feature/add models

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(
